e761d9e5ef0f08da359d87db7f311713753c9628
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-15 11:24:24 +0300

    sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+  
+  def get_value(self):
+    return self._value
+  
+  def set_value(self, new_value):
+    if isinstance(new_value, str):
+      self._value = new_value
+    else:
+      print("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+  
+  def is_question(self):
+    return self._value.endswith("?")
+  
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences(self):
+    value = self.value
+    for mark in ["!", "?", ","]:
+      value = value.replace(mark, ".")
+    all_sentences = [x for x in value.split(".") if x]
+    return len(all_sentences)
